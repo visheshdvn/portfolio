186a31093583c341e8690a1f5fde8f15df1d733e
visheshdvn <vishesh.dh1@gmail.com>
2021-11-09 13:15:26 +0530

    links assigned in sidebar

--- components/SideBar.js
+++ components/SideBar.js
@@ -1,24 +1,49 @@
-import React from 'react'
+import React from "react";
+import Link from "next/link";
 
 const SideBar = () => {
-    return (
-        <nav className="bg-black h-screen left-0 fixed lg:w-36">
-            <div className="h-full w-full relative">
-                <div className="w-full h-auto flex justify-center mt-14">
-                    <img src="namelogo-white.svg" className="w-16 h-16" />
-                </div>
+  return (
+    <nav className="bg-black h-screen left-0 fixed lg:w-36">
+      <div className="h-full w-full relative">
+        <div className="w-full h-auto flex justify-center mt-14">
+          <img src="namelogo-white.svg" className="w-16 h-16" />
+        </div>
 
-                <ul className="list-none absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
-                    <li className="mb-10 text-xl font-secondary transform transition-all duration-300 hover:text-theme-about">About</li>
-                    <li className="mb-10 text-xl font-secondary transform transition-all duration-300 hover:text-theme-resume">Resume</li>
-                    <li className="mb-10 text-xl font-secondary transform transition-all duration-300 hover:text-theme-projects">Projects</li>
-                    <li className="mb-10 text-xl font-secondary transform transition-all duration-300 hover:text-theme-blog">Blog</li>
-                    <li className="mb-10 text-xl font-secondary transform transition-all duration-300 hover:text-theme-skills">Skills</li>
-                    <li className="mb-16 text-xl font-secondary transform transition-all duration-300 hover:text-theme-contact">Contact</li>
-                </ul>
-            </div>
-        </nav>
-    )
-}
+        <ul className="list-none absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+          <Link href="/#home">
+            <li className="mb-10 text-xl font-secondary transform transition-all duration-300 hover:text-theme-about">
+              About
+            </li>
+          </Link>
+          <Link href="/#resume">
+            <li className="mb-10 text-xl font-secondary transform transition-all duration-300 hover:text-theme-resume">
+              Resume
+            </li>
+          </Link>
+          <Link href="/#projects">
+            <li className="mb-10 text-xl font-secondary transform transition-all duration-300 hover:text-theme-projects">
+              Projects
+            </li>
+          </Link>
+          <Link href="/#blog">
+            <li className="mb-10 text-xl font-secondary transform transition-all duration-300 hover:text-theme-blog">
+              Blog
+            </li>
+          </Link>
+          <Link href="/#skills">
+            <li className="mb-10 text-xl font-secondary transform transition-all duration-300 hover:text-theme-skills">
+              Skills
+            </li>
+          </Link>
+          <Link href="/#contact">
+            <li className="mb-16 text-xl font-secondary transform transition-all duration-300 hover:text-theme-contact">
+              Contact
+            </li>
+          </Link>
+        </ul>
+      </div>
+    </nav>
+  );
+};
 
-export default SideBar
+export default SideBar;
