624e9c8c703d616f48f2f2e47ccb13e8bb6b9a19
visheshdvn <vishesh.dh1@gmail.com>
2021-11-10 22:17:26 +0530

    home changes

--- components/SideBar.js
+++ components/SideBar.js
@@ -6,37 +6,39 @@ const SideBar = () => {
     <nav className="bg-black h-screen left-0 fixed lg:w-36">
       <div className="h-full w-full relative">
         <div className="w-full h-auto flex justify-center mt-14">
-          <img src="namelogo-white.svg" className="w-16 h-16" />
+          <Link href="/#home">
+            <img src="namelogo-white.svg" className="w-16 h-16 cursor-pointer" />
+          </Link>
         </div>
 
         <ul className="list-none absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
-          <Link href="/#home">
-            <li className="mb-10 text-xl font-secondary transform transition-all duration-300 hover:text-theme-about">
+          <Link href="/#about">
+            <li className="mb-10 text-xl cursor-pointer font-secondary transform transition-all duration-300 hover:text-theme-about">
               About
             </li>
           </Link>
           <Link href="/#resume">
-            <li className="mb-10 text-xl font-secondary transform transition-all duration-300 hover:text-theme-resume">
+            <li className="mb-10 text-xl cursor-pointer font-secondary transform transition-all duration-300 hover:text-theme-resume">
               Resume
             </li>
           </Link>
           <Link href="/#projects">
-            <li className="mb-10 text-xl font-secondary transform transition-all duration-300 hover:text-theme-projects">
+            <li className="mb-10 text-xl cursor-pointer font-secondary transform transition-all duration-300 hover:text-theme-projects">
               Projects
             </li>
           </Link>
           <Link href="/#blog">
-            <li className="mb-10 text-xl font-secondary transform transition-all duration-300 hover:text-theme-blog">
+            <li className="mb-10 text-xl cursor-pointer font-secondary transform transition-all duration-300 hover:text-theme-blog">
               Blog
             </li>
           </Link>
           <Link href="/#skills">
-            <li className="mb-10 text-xl font-secondary transform transition-all duration-300 hover:text-theme-skills">
+            <li className="mb-10 text-xl cursor-pointer font-secondary transform transition-all duration-300 hover:text-theme-skills">
               Skills
             </li>
           </Link>
           <Link href="/#contact">
-            <li className="mb-16 text-xl font-secondary transform transition-all duration-300 hover:text-theme-contact">
+            <li className="mb-16 text-xl cursor-pointer font-secondary transform transition-all duration-300 hover:text-theme-contact">
               Contact
             </li>
           </Link>
--- components/header.js
+++ components/header.js
@@ -1,20 +1,37 @@
 export default function Header() {
-    return (
-        <header
-            id="home"
-            style={{
-                backgroundImage: "linear-gradient(to bottom, rgba(16,16,16,0.3), rgba(16,16,16,0.3)), url(headerbg.jpg)",
-                backgroundSize: "cover",
-                backgroundPosition: "center",
-            }}
-            className="min-h-screen relative"
-        >
+  return (
+    <header
+      id="home"
+      style={{
+        backgroundImage:
+          "linear-gradient(to bottom, rgba(16,16,16,0.3), rgba(16,16,16,0.3)), url(headerbg.jpg)",
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+      }}
+      className="min-h-screen relative"
+    >
+      <div className="absolute top-1/2 transform -translate-y-1/2 left-14 font-secondary">
+        <h2 className="text-3xl mb-2">Hi! I am</h2>
+        <h1 className="font-primary text-8xl mb-2">Vishesh Dhawan</h1>
+        <h2 className="text-3xl">
+          & I am a <span className="text-blue-600">Web Developer</span>
+        </h2>
+      </div>
+      <div className="absolute right-10 top-1/2 transform -translate-y-1/2">
+        <SocialButtons media="linkedin" />
+        <SocialButtons media="github" />
+        <SocialButtons media="twitter" />
+        <SocialButtons media="leetcode" />
+        <SocialButtons media="lumbytes" />
+      </div>
+    </header>
+  );
+}
 
-            <div className="absolute top-1/2 transform -translate-y-1/2 left-14 font-secondary">
-                <h2 className="text-3xl mb-2">Hi! I am</h2>
-                <h1 className="font-primary text-8xl mb-2">Vishesh Dhawan</h1>
-                <h2 className="text-3xl">& I am a <span className="text-blue-600">Web Developer</span></h2>
-            </div>
-        </header>
-    );
+function SocialButtons({ media }) {
+  return (
+    <div className="social-icons-header cursor-pointer rounded-full transform transition-all duration-300 overflow-hidden hover:bg-gray-800">
+      <img src={`/icons/${media}.svg`} alt={`${media} icon`} className="" />
+    </div>
+  );
 }
